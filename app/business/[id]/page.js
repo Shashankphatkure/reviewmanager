c2837cf35bc5072a40d578847f71ad770000a4f2
Shashank Phatkure <shashankphatkure@gmail.com>
2024-09-09 01:56:54 +0530

    updated buttons

--- app/business/[id]/page.js
+++ app/business/[id]/page.js
@@ -249,12 +249,7 @@ const BusinessPage = ({ params }) => {
           </form>
 
           <div className="mt-8 flex justify-between items-center">
-            <button
-              onClick={() => router.push('/dashboard')}
-              className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600 transition duration-300"
-            >
-              Back to Dashboard
-            </button>
+            
             {business.upi_id && (
               <button
                 onClick={() => window.open(`upi://pay?pa=${business.upi_id}&pn=${business.name}&cu=INR`, "_blank")}
